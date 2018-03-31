921856cb741463de00c03c407abfa0f09a70ed23
Roel de Jong <Roel4811@hotmail.com>
2018-03-31 15:20:19 +0200

    Return array of hashes

--- app/services/bol_scraper_service.rb
+++ app/services/bol_scraper_service.rb
@@ -3,35 +3,45 @@ require 'open-uri'
 
 class BolScraperService
 
+  attr_reader :url
+
+  def initialize(url)
+    @url = url
+  end
+
   def call
-    url = "https://www.bol.com/nl/l/anker-powerbanks/N/18135/filter_N/4294856588/?page=2&view=tiles"
+    # url = "https://www.bol.com/nl/l/anker-powerbanks/N/18135/filter_N/4294856588/?page=2&view=tiles"
     response = open(url)
     html = Nokogiri::HTML(response)
 
+    records = []
     products = html.search('#js_items_content li.product-item--column')
     products.each_with_index do |pr, i|
-      name = pr.search('a.product-title').text.strip
-      store_id = pr.attr("data-id").to_i
-      image = pr.search('a.product-image img').attr('src').text.strip
-      link = pr.search('a.product-image').attr('href').value
-      price = html.xpath("//*[@id='js_items_content']/li[#{i+1}]/div[2]/div[2]/span/div/div/div/meta").attr("content").value.to_f
-      rating = pr.search('.rating-stars').attr('title').value
-      review_amount = pr.search('.rating-stars__rating-count').text.split('').map {|x| x[/\d+/]}.join.to_i
-      available = html.xpath("//*[@id='js_items_content']/li[#{i+1}]/div[2]/div[3]/div/text()").text.blank? ? false : true
-      availability = html.xpath("//*[@id='js_items_content']/li[#{i+1}]/div[2]/div[3]/div/text()").text.strip
+      product = {}
+      product['name'] = pr.search('a.product-title').text.strip
+      product['store_id'] = pr.attr("data-id").to_i
+      product['image'] = pr.search('a.product-image img').attr('src').text.strip
+      product['link'] = pr.search('a.product-image').attr('href').value
+      product['price'] = html.xpath("//*[@id='js_items_content']/li[#{i+1}]/div[2]/div[2]/span/div/div/div/meta").attr("content").value.to_f
+      product['rating'] = pr.search('.rating-stars').attr('title').value
+      product['review_amount'] = pr.search('.rating-stars__rating-count').text.split('').map {|x| x[/\d+/]}.join.to_i
+      product['available'] = html.xpath("//*[@id='js_items_content']/li[#{i+1}]/div[2]/div[3]/div/text()").text.blank? ? false : true
+      product['availability'] = html.xpath("//*[@id='js_items_content']/li[#{i+1}]/div[2]/div[3]/div/text()").text.strip
 
-      product = Product.find_or_initialize_by(store_id: store_id)
-      product.name = name if name.present?
-      product.image = image if image.present?
-      product.link = link if link.present?
-      product.price = price if price.present?
-      product.rating = rating if rating.present?
-      product.review_amount = review_amount if review_amount.present?
-      product.available = available if available.present?
-      product.availability = availability if availability.present?
-
-      product.save if product.new_record? || product.changed?
+      records << product
+      # product = Product.find_or_initialize_by(store_id: store_id)
+      # product.name = name if name.present?
+      # product.image = image if image.present?
+      # product.link = link if link.present?
+      # product.price = price if price.present?
+      # product.rating = rating if rating.present?
+      # product.review_amount = review_amount if review_amount.present?
+      # product.available = available if available.present?
+      # product.availability = availability if availability.present?
+      #
+      # product.save if product.new_record? || product.changed?
     end
+    records
   end
 end
 
