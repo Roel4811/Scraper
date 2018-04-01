2083faf2c235f7075ea3c1981807dde731638bda
Roel de Jong <Roel4811@hotmail.com>
2018-04-01 23:59:56 +0200

    Add authentication

--- app/controllers/admin/auth_controller.rb
+++ app/controllers/admin/auth_controller.rb
@@ -1,2 +1,18 @@
-class Admin::PagesController < Admin::ApplicationController
+class Admin::AuthController < Admin::ApplicationController
+  before_action :only_for_anonymous # check if we know the user
+
+  def new; end
+
+  # Get username from params, save to session and redirect to admin_root
+  def create
+    session[:username] = params[:username]
+    redirect_to admin_root_path
+  end
+
+  private
+
+  # If a user has been here before — send them straight to admin_root
+  def only_for_anonymous
+    redirect_to admin_root_path if session[:username]
+  end
 end
--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,8 +1,17 @@
 class Admin::PagesController < Admin::ApplicationController
+  before_action :authenticate!
+
   def home
   end
 
   def database
     @products = Product.all
+  end
+
+  private
+
+  # redirect user to /login if he hadn't picked a username yet
+  def authenticate!
+    redirect_to admin_login_path unless session[:username]
   end
 end
