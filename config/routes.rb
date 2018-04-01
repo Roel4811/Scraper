2a157c9197b64cdb5dbefcbf47e49d2009bd5011
Roel de Jong <Roel4811@hotmail.com>
2018-04-01 23:59:21 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: "pages#home"
+
+    get  "/login", to: "auth#new"
+    post "/login", to: "auth#create"
+
     get '/scraper-input', to: "application#scraper_input"
     get '/database', to: "pages#database"
     post '/create-products', to: "products#create_products"
