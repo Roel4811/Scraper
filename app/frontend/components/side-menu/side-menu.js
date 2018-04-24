ff7af16305b862679a920673fef828e9cd3e6db7
Roel de Jong <Roel4811@hotmail.com>
2018-04-24 22:41:12 +0200

    Submit form with Rails ujs to invoke js

--- app/frontend/components/side-menu/side-menu.js
+++ app/frontend/components/side-menu/side-menu.js
@@ -1,1 +1,13 @@
+import Rails from 'rails-ujs';
 import "./side-menu.scss";
+
+$(document).ready(function() {
+  document.getElementById("provider").addEventListener("change", submitForm);
+  document.getElementById("brand").addEventListener("change", submitForm);
+  submitForm();
+});
+
+function submitForm() {
+  var form = document.querySelector('form');
+  Rails.fire(form, 'submit');
+}
