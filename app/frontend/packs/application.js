9aef5c0f1ff05c6afe69e2a770eb3ef000af4bf8
Roel de Jong <Roel4811@hotmail.com>
2018-04-24 22:41:39 +0200

    Imports

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,7 +1,10 @@
 import Rails from 'rails-ujs';
-import "init";
 import 'bootstrap/dist/js/bootstrap';
+//
+// import "../components/navbar/navbar";
 
-import "../components/navbar/navbar";
+// import "../components/search-results/search-results";
 
 Rails.start();
+
+import "../components/side-menu/side-menu";
