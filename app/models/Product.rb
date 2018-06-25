768e4ecc1cdca073f3f57aafed0d50e5557ecf24
Roel de Jong <Roel4811@hotmail.com>
2018-06-25 23:25:44 +0200

    Add price range constant

--- app/models/Product.rb
+++ app/models/Product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   belongs_to :provider
 
+  PRICE_RANGES = ["0-10", "10-20", "20-30", ">30"]
+
   def provider_name
     self.provider.name if self.provider
   end
