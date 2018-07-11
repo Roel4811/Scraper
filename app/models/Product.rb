a0024cfc0af4af9cf2fe9207e5103acb439d9b5b
Roel de Jong <Roel4811@hotmail.com>
2018-07-11 19:19:41 +0200

    extend model for elasticsearch

--- app/models/Product.rb
+++ app/models/Product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+  
   belongs_to :provider
 
   PRICE_RANGES = ["0-10", "10-20", "20-30", ">30"]
