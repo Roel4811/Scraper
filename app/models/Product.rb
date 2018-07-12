63575683a709b6813857fe20e772ccdca5593c68
Roel de Jong <Roel4811@hotmail.com>
2018-07-12 09:42:47 +0200

    Add mappings

--- app/models/Product.rb
+++ app/models/Product.rb
@@ -1,7 +1,24 @@
 class Product < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
-  
+
+  settings do
+    mappings dynamic: false do
+      indexes :name, type: :text, analyzer: :english
+      indexes :price, type: :integer
+      indexes :image, type: :text
+      indexes :link, type: :text
+      indexes :rating, type: :text
+      indexes :review_amount, type: :integer
+      indexes :store_id, type: :integer
+      indexes :available, type: :boolean
+      indexes :availability, type: :text, analyzer: :english
+      indexes :provider_id, type: :integer
+      indexes :brand, type: :text, analyzer: :english
+      indexes :issue, type: :boolean
+    end
+  end
+
   belongs_to :provider
 
   PRICE_RANGES = ["0-10", "10-20", "20-30", ">30"]
