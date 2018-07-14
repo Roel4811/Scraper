a9a31aef2fbd1e25a2bd3d7cc3c519bc0289e0e2
Roel de Jong <Roel4811@hotmail.com>
2018-07-14 20:04:59 +0200

    Update search terms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
 class ProductsController < ApplicationController
   def index
     @providers = Provider.all
-    @products = Product.all
-    @brands = @products.distinct.pluck(:brand).select(&:present?)
+    @products = Product.search_products("*")
+    @brands = Product.distinct.pluck(:brand).select(&:present?)
   end
 
   def show
@@ -12,7 +12,13 @@ class ProductsController < ApplicationController
   def search_products
     provider = params[:provider]
     brand = params[:brand]
-
-    @records = Product.joins(:provider).where(providers: { name: provider }).where(brand: brand)
+    query = params[:query]
+    if query.present?
+      @records = Product.search_products(query)
+    else
+      # @records = Product.joins(:provider).where(providers: { name: provider }).where(brand: brand)
+      @records = Product.search_products("#{provider} #{brand}")
+    end
   end
+
 end
