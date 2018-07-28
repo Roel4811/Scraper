a4a84e817b2694a3a803a66f60a4b75cc364769b
Roel de Jong <Roel4811@hotmail.com>
2018-07-28 13:17:30 +0200

    Initialize nice select

--- app/frontend/components/side-menu/side-menu.js
+++ app/frontend/components/side-menu/side-menu.js
@@ -1,13 +1,28 @@
 import Rails from 'rails-ujs';
 import "./side-menu.scss";
 
-$(document).ready(function() {
-  document.getElementById("provider").addEventListener("change", submitForm);
-  document.getElementById("brand").addEventListener("change", submitForm);
-  submitForm();
-});
+// import 'jquery-nice-select';
+
+// $(document).ready(function() {
+//   document.getElementById("provider").addEventListener("change", submitForm);
+//   document.getElementById("brand").addEventListener("change", submitForm);
+//   submitForm();
+// });
 
 function submitForm() {
   var form = document.querySelector('form');
   Rails.fire(form, 'submit');
 }
+
+function initNiceSelect() {
+  const selectBoxes = document.querySelectorAll(".custom-select")
+  selectBoxes.forEach((select_box) => {
+    select_box.niceSelect();
+  });
+}
+
+// initNiceSelect();
+// import 'jquery-nice-select/dist/css/nice-select.css';
+
+
+// export { initNiceSelect };
