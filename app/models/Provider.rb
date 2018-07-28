47875a12effb75b2396a5742ba7d8035360620b2
Roel de Jong <Roel4811@hotmail.com>
2018-07-28 13:15:19 +0200

    Map provider for ES

--- app/models/Provider.rb
+++ app/models/Provider.rb
@@ -1,3 +1,12 @@
 class Provider < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  settings do
+    mappings dynamic: false do
+      indexes :name, type: :text, analyzer: :english
+    end
+  end
+
   has_many :products
 end
