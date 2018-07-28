54944ffea8829739c7817f5a59088a7775f87502
Roel de Jong <Roel4811@hotmail.com>
2018-07-28 13:16:09 +0200

    Import pretty checkbox variables

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,10 +1,14 @@
 import Rails from 'rails-ujs';
 import 'bootstrap/dist/js/bootstrap';
-//
-// import "../components/navbar/navbar";
+// import 'jquery-nice-select/dist/css/nice-select.css';
+
+import "../components/navbar/navbar";
 
 // import "../components/search-results/search-results";
+import '../components/select-box/select-box';
 
 Rails.start();
 
 import "../components/side-menu/side-menu";
+// import { initNiceSelect } from '../components/side-menu/side-menu';
+// initNiceSelect();
