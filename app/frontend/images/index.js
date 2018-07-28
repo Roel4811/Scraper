132a4ff67b567b00f45fb9d7fe69a14041849253
Roel de Jong <Roel4811@hotmail.com>
2018-07-28 13:16:38 +0200

    Import images

--- app/frontend/images/index.js
+++ app/frontend/images/index.js
@@ -2,3 +2,7 @@ import './telescope.svg';
 import './purple_background.svg';
 import './telescope_with_text.svg';
 import './powerbank_with_hand.svg';
+import './background_with_powerbank';
+import './logo_bol';
+import './logo_coolblue';
+import './logo_text';
