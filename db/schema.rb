4aad58fdaabd922effbd1e867a3426350b5e3608
Roel de Jong <Roel4811@hotmail.com>
2018-07-28 12:56:56 +0200

    Add searcg model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411182618) do
+ActiveRecord::Schema.define(version: 20180728104841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20180411182618) do
 
   create_table "providers", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "keywords"
+    t.decimal "min_price"
+    t.decimal "max_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "products", "providers"
