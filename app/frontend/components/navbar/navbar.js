558cdb7f22ade33e35d85f0d40e0b6ec2d165689
Roel de Jong <Roel4811@hotmail.com>
2018-07-28 13:18:05 +0200

    Update navbar

--- app/frontend/components/navbar/navbar.js
+++ app/frontend/components/navbar/navbar.js
@@ -1,8 +1,35 @@
 window.onscroll = function() {
   var nav = document.getElementById('nav');
-  if ( window.pageYOffset > 100 ) {
-      nav.classList.add("scrolled");
+  var navLinks = document.querySelectorAll('.nav-item');
+  var navBrand = document.getElementById('navbar-brand');
+  // console.log(window.pageYOffset);
+  if ( window.pageYOffset > 150 ) {
+    nav.classList.add("scrolled");
+    addBlack(navLinks);
+    removeOpacity(navBrand);
   } else {
-      nav.classList.remove("scrolled");
+    nav.classList.remove("scrolled");
+    removeBlack(navLinks);
+    addOpacity(navBrand);
   }
+}
+
+function addBlack(items) {
+  items.forEach((item) => {
+    item.classList.add("black");
+  });
+}
+
+function removeBlack(items) {
+  items.forEach((item) => {
+    item.classList.remove("black");
+  });
+}
+
+function addOpacity(item) {
+  item.classList.add("add-opacity");
+}
+
+function removeOpacity(item) {
+  item.classList.remove("add-opacity");
 }
