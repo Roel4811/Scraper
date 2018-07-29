2a40ccbd8bdb3003b6df1dbb480808411261ba1d
Roel de Jong <Roel4811@hotmail.com>
2018-07-29 22:56:53 +0200

    Remove elastic search content

--- app/models/Product.rb
+++ app/models/Product.rb
@@ -1,24 +1,5 @@
 class Product < ActiveRecord::Base
   searchkick
-  # include Elasticsearch::Model
-  # include Elasticsearch::Model::Callbacks
-
-  # settings do
-  #   mappings dynamic: false do
-  #     indexes :name, type: :text, analyzer: :english
-  #     indexes :price, type: :integer
-  #     indexes :image, type: :text
-  #     indexes :link, type: :text
-  #     indexes :rating, type: :text
-  #     indexes :review_amount, type: :integer
-  #     indexes :store_id, type: :keyword
-  #     indexes :available, type: :boolean
-  #     indexes :availability, type: :text, analyzer: :english
-  #     indexes :provider_id, type: :integer
-  #     indexes :brand, type: :text, analyzer: :english
-  #     indexes :issue, type: :boolean
-  #   end
-  # end
 
   belongs_to :provider
 
@@ -30,59 +11,11 @@ class Product < ActiveRecord::Base
     Product.distinct.pluck(:brand).select(&:present?)
   end
 
-  # def self.search_on_keywords(query)
-  #   self.search({
-  #     from: 0,
-  #     size: 40,
-  #     query: {
-  #       bool: {
-  #         must: [
-  #         {
-  #           multi_match: {
-  #             query: query,
-  #             fields: [:name, :availability, :brand]
-  #           }
-  #         },
-  #         {
-  #           match: {
-  #             available: true
-  #           }
-  #         }]
-  #       }
-  #     },
-  #     highlight: {
-  #       pre_tags: ['<em><strong>'],
-  #       post_tags: ['</em></strong>'],
-  #       fields: {
-  #         :"*" => {}
-  #       }
-  #     }
-  #   })
-  # end
-
-  # def self.search_on_price_range(min_price, max_price)
-  #   self.search({
-  #     from: 0,
-  #     size: 100,
-  #     query: {
-  #       range: {
-  #         price: {
-  #           gte: min_price,
-  #           lte: max_price
-  #         }
-  #       }
-  #     },
-  #     highlight: {
-  #       pre_tags: ['<em><strong>'],
-  #       post_tags: ['</em></strong>'],
-  #       fields: {
-  #         :"*" => {}
-  #       }
-  #     }
-  #   })
-  # end
-
   def self.whole_search(keywords, min_price, max_price)
+    keywords = "*" unless keywords.present?
+    min_price = 0 unless min_price.present?
+    max_price = 1000 unless max_price.present?
+
     self.search(keywords, where: {
         price: min_price..max_price
       }, operator: "or", load: false)
