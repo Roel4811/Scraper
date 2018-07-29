988aa33c61ed6a2aaf9a89e0b01a8304677b3da2
Roel de Jong <Roel4811@hotmail.com>
2018-07-29 12:53:43 +0200

    Add search actions

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,11 @@
 class SearchesController < ApplicationController
+  def create
+    binding.pry
+    @search = Search.create!(params[:search])
+    @records = @search.products
+  end
+
+  def show
+    @search = Search.find(params[:id])
+  end
 end
