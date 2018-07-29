81d7d15f7ee73a0afd433419835c0a4a08e946d2
Roel de Jong <Roel4811@hotmail.com>
2018-07-29 12:53:33 +0200

    Update search model

--- app/models/Search.rb
+++ app/models/Search.rb
@@ -1,2 +1,49 @@
 class Search < ApplicationRecord
+
+  def products
+    @products ||= find_products
+  end
+
+  private
+
+  def find_products
+    products = Product.order(:name)
+    # products = products.where("name like ?", "%#{keywords}%") if keywords.present?
+    # products = products.where(category_id: category_id) if category_id.present?
+    # products = products.where("price >= ?", min_price) if min_price.present?
+    # products = products.where("price <= ?", max_price) if max_price.present?
+    products = products.search_products(brands.join(" ")) if brands.present?
+    products
+  end
+
+  def self.search_products(query)
+    Product.search({
+      from: 0,
+      size: 40,
+      query: {
+        bool: {
+          must: [
+          {
+            multi_match: {
+              query: query,
+              fields: [:name, :availability, :brand]
+            }
+          },
+          {
+            match: {
+              available: true
+            }
+          }]
+        }
+      },
+      highlight: {
+        pre_tags: ['<em><strong>'],
+        post_tags: ['</em></strong>'],
+        fields: {
+          name: {}
+        }
+      }
+    })
+  end
+
 end
