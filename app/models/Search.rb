ad5b5b04ec6da288139d5f4939e2d16207f846b9
Roel de Jong <Roel4811@hotmail.com>
2018-07-30 22:48:35 +0200

    Update callbacks for providers

--- app/models/Search.rb
+++ app/models/Search.rb
@@ -4,10 +4,12 @@ class Search < ApplicationRecord
 
   def remove_brackets
     self.brands.gsub!(/[\[\]\"]/, "") if attribute_present?("brands")
+    self.providers.gsub!(/[\[\]\"]/, "") if attribute_present?("providers")
   end
 
   def remove_commas
     self.brands.gsub!(/\,/,"") if attribute_present?("brands")
+    self.providers.gsub!(/\,/,"") if attribute_present?("providers")
   end
 
   def products
@@ -22,8 +24,7 @@ class Search < ApplicationRecord
     # products = products.where(category_id: category_id) if category_id.present?
     # products = products.where("price >= ?", min_price) if min_price.present?
     # products = products.where("price <= ?", max_price) if max_price.present?
-    # binding.pry
-    products = products.whole_search(brands, min_price, max_price)
+    products = products.whole_search(providers, brands, min_price, max_price)
     # products = products.search_on_keywords(brands).records if brands.present?
     # products = products.search_on_price_range(min_price, max_price) if (min_price.present? && max_price.present?)
     products
