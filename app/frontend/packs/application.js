13c0f36ebcfaf65bfe242ce4670d2faeaff2976f
Roel de Jong <Roel4811@hotmail.com>
2018-08-05 13:39:02 +0200

    Change imports

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,13 +1,17 @@
 import Rails from 'rails-ujs';
+Rails.start();
+
 import 'bootstrap/dist/js/bootstrap';
 // import 'jquery-nice-select/dist/css/nice-select.css';
+import 'bootstrap-autocomplete-input-assets/src/js/bootstrap3-typeahead.min.js';
+import 'bootstrap-autocomplete-input-assets/src/js/bootstrap-autocomplete-input.min.js';
 
 import "../components/navbar/navbar";
 
 // import "../components/search-results/search-results";
 import '../components/select-box/select-box';
 
-Rails.start();
+
 
 import "../components/side-menu/side-menu";
 // import { initNiceSelect } from '../components/side-menu/side-menu';
