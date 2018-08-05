b0ec48891897022acf28db8991bfd21b1761ec38
Roel de Jong <Roel4811@hotmail.com>
2018-08-05 13:05:28 +0200

    Add jquery plugin

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,7 +4,9 @@ const webpack = require('webpack');
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
+  jquery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default'],
 }));
 
 module.exports = environment;
