94a31c949284a8beac241efc7100b30795bc4412
Roel de Jong <Roel4811@hotmail.com>
2018-10-01 19:00:58 +0200

    Add associations

--- app/models/Search.rb
+++ app/models/Search.rb
@@ -1,4 +1,6 @@
 class Search < ApplicationRecord
+  has_and_belongs_to_many :providers
+  has_and_belongs_to_many :brands
 
   before_save :remove_brackets, :remove_commas
 
@@ -13,18 +15,18 @@ class Search < ApplicationRecord
   end
 
   def products
-    @products ||= find_products
+    find_products
   end
 
   private
 
   def find_products
-    products = Product.order(:name)
+    # products = Product.order(:name)
     # products = products.where("name like ?", "%#{keywords}%") if keywords.present?
     # products = products.where(category_id: category_id) if category_id.present?
     # products = products.where("price >= ?", min_price) if min_price.present?
     # products = products.where("price <= ?", max_price) if max_price.present?
-    products = products.whole_search(providers, brands, min_price, max_price)
+    products = Product.whole_search(keywords, providers, brands, min_price, max_price)
     # products = products.search_on_keywords(brands).records if brands.present?
     # products = products.search_on_price_range(min_price, max_price) if (min_price.present? && max_price.present?)
     products
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,4 @@
-class Provider < ActiveRecord::Base
+class Brand < ApplicationRecord
   has_many :products
   has_and_belongs_to_many :searches
 end
