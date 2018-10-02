cc79e528583b78e4da00d4ed450d3419bde715ca
Roel de Jong <Roel4811@hotmail.com>
2018-10-02 16:37:44 +0200

    Update imports

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -6,7 +6,5 @@ import './src/application.scss';
 
 import "../components/navbar/navbar";
 import '../components/select-box/select-box';
-import "../components/side-menu/side-menu";
+// import "../components/side-menu/side-menu";
 import "../components/page/page";
-// import { initNiceSelect } from '../components/side-menu/side-menu';
-// initNiceSelect();
