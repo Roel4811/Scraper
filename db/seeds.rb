eda45e807031c01a6918b72fd25a3cf4bf22d87f
Roel de Jong <Roel4811@hotmail.com>
2018-10-02 16:34:43 +0200

    Update seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Provider.delete_all
+Brand.delete_all
+
 providers = Provider.create([{ name: 'coolblue' }, { name: 'bol' }])
 brands = Brand.create([
   {
