0995df907d47fb29d6ac3aa4e50e76682a382072
Roel de Jong <Roel4811@hotmail.com>
2018-10-07 17:00:47 +0200

    Search with scopes instead of ES

--- app/models/Product.rb
+++ app/models/Product.rb
@@ -1,14 +1,24 @@
 class Product < ActiveRecord::Base
-  searchkick word_start: [:name]
-  scope :search_import, -> { includes(:provider) }
+  # searchkick word_middle: [:name]
+  # scope :search_import, -> { includes(:provider) }
 
   belongs_to :provider
+  belongs_to :brand
+
+  scope :search_by_keywords, -> (*keywords) { binding.pry }
+
+  scope :products_by_keywords, -> (*keywords) { where((["lower(products.name) like ?"] * keywords.size).join(' OR '), *keywords.map { |keyword| "%#{keyword}%" }) }
+  scope :products_with_providers, -> (*providers) { joins(:provider).where((["lower(providers.name) like ?"] * providers.size).join(' OR '), *providers.map { |provider| "%#{provider}%" }) }
+  scope :products_with_brands, -> (*brands) { joins(:brand).where((["lower(brands.name) like ?"] * brands.size).join(' OR '), *brands.map { |brand| "%#{brand}%" }) }
+  scope :products_with_min_price, -> (min_price) { where("products.price >= ?", min_price) }
+  scope :products_with_max_price, -> (max_price) { where("products.price <= ?", max_price) }
 
   def search_data
     {
       name: name,
       provider: provider.name,
-      brand: brand
+      brand: brand,
+      price: price
     }
   end
 
@@ -16,26 +26,22 @@ class Product < ActiveRecord::Base
     self.provider.name if self.provider
   end
 
-  def self.present_brands
-    Product.distinct.pluck(:brand).select(&:present?)
+  def brand_name
+    self.brand.name if self.brand
   end
 
-  def self.whole_search(providers, brands, min_price, max_price)
+  def self.whole_search(keywords, providers, brands, min_price, max_price)
     keywords = "*" unless keywords.present?
-    providers = "" unless providers.present?
-    brands = "" unless brands.present?
+    providers = "*" unless providers.present?
+    brands = "*" unless brands.present?
     min_price = 0 unless min_price.present?
     max_price = 1000 unless max_price.present?
 
-    products_by_keywords = Product.search(keywords, operator: "or", execute: false)
-    products_by_providers = Product.search(providers, operator: "or", execute: false)
-    products_by_brand = Product.search(brands, operator: "or", execute: false)
+    products = Product.search(keywords, where: {price: {gt: min_price, lt: max_price}}).results &
+    Product.search(providers, fields: [:provider], operator: "or").results &
+    Product.search(brands, operator: "or").results
 
-    binding.pry
-
-    Searchkick.multi_search([products_by_keywords, products_by_providers, products_by_brand], where: {
-        price: min_price..max_price
-      }, operator: "or", load: false)
+    products
   end
 
   def self.search_brands(brand)
