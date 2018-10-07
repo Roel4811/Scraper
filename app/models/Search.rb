ff2b159d2d15f46b41d44940f35aa421cedfbb44
Roel de Jong <Roel4811@hotmail.com>
2018-10-07 17:00:57 +0200

    Add validators

--- app/models/Search.rb
+++ app/models/Search.rb
@@ -2,9 +2,9 @@ class Search < ApplicationRecord
   has_and_belongs_to_many :providers, uniq: true
   has_and_belongs_to_many :brands, uniq: true
 
-  validates :keywords, presence: true, no_html: true
-  validates :min_price, presence: true, no_html: true
-  validates :max_price, presence: true, no_html: true
+  validates :keywords, no_html: true
+  # validates :min_price, no_html: true
+  # validates :max_price, no_html: true
 
   def products
     find_products
@@ -13,16 +13,20 @@ class Search < ApplicationRecord
   private
 
   def find_products
-    # products = Product.order(:name)
-    # products = products.where("name like ?", "%#{keywords}%") if keywords.present?
-    # products = products.where(category_id: category_id) if category_id.present?
-    # products = products.where("price >= ?", min_price) if min_price.present?
-    # products = products.where("price <= ?", max_price) if max_price.present?
-    brands = self.brands.map(&:name).join(" ")
-    providers = self.providers.map(&:name).join(" ")
-    products = Product.whole_search(keywords, providers, brands, min_price, max_price)
-    # products = products.search_on_keywords(brands).records if brands.present?
-    # products = products.search_on_price_range(min_price, max_price) if (min_price.present? && max_price.present?)
+    brands = self.brands.map(&:name)
+    providers = self.providers.map(&:name)
+    keywords = self.keywords.split(" ").map(&:downcase) if keywords.present?
+
+    products = Product.all
+
+    binding.pry
+    products = products.products_by_keywords(*keywords) if keywords.present?
+    products = products.products_with_providers(*providers) if providers.present?
+    products = products.products_with_brands(*brands) if brands.present?
+    products = products.products_with_min_price(min_price) if min_price.present?
+    products = products.products_with_max_price(max_price) if max_price.present?
+
+    # products = Product.whole_search(keywords, providers, brands, min_price, max_price)
     products
   end
 
