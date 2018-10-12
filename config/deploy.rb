3b46b1f29ec8b6f25f6ff1740c17338d3d72e5c4
Roel de Jong <Roel4811@hotmail.com>
2018-10-12 11:07:43 +0200

    Remove linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,11 +31,11 @@ set :assets_prefix,   'packs' # added to look for packs made by webpacker
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml, config/secrets.yml}
+# set :linked_files, %w{config/database.yml, config/secrets.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # added line
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
