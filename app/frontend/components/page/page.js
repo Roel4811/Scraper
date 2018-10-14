916a840cd2f19e582ee7654e6e9b2fda0341176b
Roel de Jong <Roel4811@hotmail.com>
2018-10-14 15:01:03 +0200

    Import footer

--- app/frontend/components/page/page.js
+++ app/frontend/components/page/page.js
@@ -6,3 +6,4 @@ import "components/main-col/main-col";
 import "components/banner/banner";
 import "components/navbar/navbar";
 import "components/searchResults/searchResults";
+import "components/footer/footer";
